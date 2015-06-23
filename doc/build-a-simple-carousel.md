2f549acf44176e560ab1ae4da2effd68d4049ca9
GYF1 <guoyinfeng@live.com>
2015-06-23 21:24:11 +0800

    backup

---
+++ doc/build-a-simple-carousel.md
@@ -0,0 +1,58 @@
+title: 'build a simple carousel'
+date: 2015-05-28 13:58:59
+categories: JavaScript
+tags: Practices
+
+---
+
+[Demo]()
+
+
+### 首先要有一个容器，容器里面放图片，但是每次只显式一张图片
+实现思路：
+每次显示一张的方法有：
+1. 固定容器大小，大小和一张图片的大小一样，然后通过控制每张图片的position来切换图片
+2. 通过display属性，控制显式与否
+
+实现中觉得第1种方法更好，第1种在切换位置的时候，可以做动态过渡效果。
+第2种方法要做过渡效果，可能就需要用css3来做动画了
+
+代码实现：
+0. 外面一个大容器，里面两个小容器，一个是图片显式，一个是图片页码的显式
+1. 容器的overflow设为hidden，这样固定宽度和高度后，其他的图片会被隐藏
+2. 图片子容器的position设为absolute, 这样才可以通过top属性改变显式的图片
+3. 父容器的position必须要是relative或者absolute, 这样容器里面的子容器，设置完absolute才可以把位置控制在父容器里面，不然直接会以浏览器窗口的位置显式子容器。
+而且父元素设置的高度和宽度也才会起作用。不然图片还是会全部显示，脱离了父元素的限制。
+
+
+### 每切换一次，相当于改变一下top的位置
+每次改变的高度是根据单张图片的高度决定的
+第一次为top:0, 第二次为top:-(height), 依此类推
+所以我们可以通过 (图片index * 图片height) 算出的值决定显示哪张图片
+
+
+### 切换的动画效果
+上面移动的是一个目标的位置，但是为了出现滑动效果，我们可以在每一页里再细化移动的距离。
+一点点的移动，就会给人滑动的效果。
+
+滑动效果用到的计算参数
+现在的位置
+目标的位置
+滑动的时间
+
+这里使用了Tween的算法
+
+    tween: function (timeUsed, currentPosition, distance, duration) {
+      return -distance * ((timeUsed = timeUsed / duration - 1) * timeUsed * timeUsed * timeUsed - 1) + currentPosition;
+    }
+    
+
+
+
+
+
+
+
+
+# THANKS
+[JavaScript 图片滑动展示效果](http://www.cnblogs.com/cloudgamer/archive/2008/05/13/1194272.html)
---
+++ src/compCarousel.js
@@ -0,0 +1,193 @@
+var compCarousel = (function (root, doc) {
+  'use strict';
+
+  var pageNums = [], timer;
+
+  //get first child skip TextNodes
+  var getFirstChild = function (el) {
+    var firstChild = el.firstChild;
+    while (firstChild !== null && firstChild.nodeType === 3) {
+      firstChild = firstChild.nextSibling;
+    }
+    return firstChild;
+  };
+
+  var extend = function (dest, src) {
+    for (var property in src) {
+      dest[property] = src[property];
+    }
+    return dest;
+  };
+
+  var currentStyle = function (element) {
+    return element.currentStyle || doc.defaultView.getComputedStyle(element, null);
+  };
+
+  var bind = function (object, fun) {
+    var args = Array.prototype.slice.call(arguments).slice(2);
+    return function () {
+      return fun.apply(object, args.concat(Array.prototype.slice.call(arguments)));
+    };
+  };
+
+  function CarouselClass(container, options) {
+
+    this._container = doc.getElementById(container);
+    this._slider = getFirstChild(this._container);
+
+    //切换数量
+    this._count = Math.abs(this._slider.children.length);
+
+    //定时器
+    this._timer = null;
+
+    //目标值
+    this._target = 0;
+
+    //tween参数
+    this.moveTimeCounter = this.currentPosition = this.moveDistance = 0;
+
+    //当前索引
+    this.Index = 0;
+
+    this.setOptions(options);
+
+    this.Auto = !!this.options.Auto;
+    this.Duration = Math.abs(this.options.Duration);
+    this.Time = Math.abs(this.options.Time);
+    this.Pause = Math.abs(this.options.Pause);
+
+    //样式设置
+    var p = currentStyle(this._container).position;
+
+    //如果不是相对或绝对定位会同时设置position为relative
+    p === 'relative' || p === 'absolute' || (this._container.style.position = 'relative');
+
+    //程序会自动设置容器overflow为hidden
+    this._container.style.overflow = 'hidden';
+
+    //滑动对象会设置为绝对定位
+    this._slider.style.position = 'absolute';
+
+    //如果没有设置Change切换参数属性，会自动根据滑动对象获取：
+    this.stepSize = this._slider.offsetHeight / this._count;
+  }
+
+
+  CarouselClass.prototype = {
+    /**
+     * @param timeUsed  时间，滑动已经用掉的时间，还有个d变量会保存允许滑动的时间
+     * @param currentPosition  初始值，当前的图片位置
+     * @param distance  变化量，目标要到的位置和现在位置的距离
+     * @param duration  允许的滑动时间总值
+     * @returns {*}
+     */
+    tween: function (timeUsed, currentPosition, distance, duration) {
+      return -distance * ((timeUsed = timeUsed / duration - 1) * timeUsed * timeUsed * timeUsed - 1) + currentPosition;
+    },
+
+    setOptions: function (options) {
+      this.options = {
+        Auto: true,
+        //滑动持续时间, 也就是从一张图变成另一张图的一共的滑动时间
+        Duration: 50,
+        //移动完一小步后停顿下
+        Time: 10,
+        //每当一个完整图片滑动完了，停顿下
+        Pause: 2000
+      };
+      extend(this.options, options || {});
+    },
+
+    run: function (index) {
+      index === undefined && (index = this.Index);
+      index < 0 && (index = this._count - 1) || index >= this._count && (index = 0);
+
+      this._target = -Math.abs(this.stepSize) * (this.Index = index);
+      this.moveTimeCounter = 0;
+      this.currentPosition = parseInt(currentStyle(this._slider).top);
+      this.moveDistance = this._target - this.currentPosition;
+
+      for (var i = 0, len = pageNums.length; i < len; i++) {
+        if (this.Index === i) {
+          pageNums[i].className = 'active';
+        } else {
+          pageNums[i].className = '';
+        }
+      }
+
+      this.move();
+    },
+
+    /**
+     * 切换一个图片的时候，这个方法会被多次的调用
+     * 从而形成图片有切换的动态效果
+     *
+     * Duration(持续时间)是自定义属性。
+     * 参数设置好后就执行Move程序开始移动了。
+     * 里面很简单，首先判断_c是否有值（等于0表示不需要移动）和_t是否到达Duration，
+     * 未满足条件就继续移动，否则直接移动到目标值并进行下一次切换：
+     */
+    move: function () {
+      clearTimeout(this._timer);
+      if (this.moveDistance && this.moveTimeCounter < this.Duration) {
+        var tweenval = this.tween(this.moveTimeCounter++, this.currentPosition, this.moveDistance, this.Duration);
+        this.moveSomeDistance(Math.round(tweenval));
+        this._timer = setTimeout(bind(this, this.move), this.Time);
+      } else {
+        this.moveSomeDistance(this._target);
+        this.Auto && (this._timer = setTimeout(bind(this, this.next), this.Pause));
+      }
+    },
+
+    moveSomeDistance: function (i) {
+      this._slider.style.top = i + 'px';
+    },
+
+    next: function () {
+      this.run(++this.Index);
+    },
+
+    addPageNumbers: function () {
+      var that = this;
+      var ulPageNums = doc.createElement('ul');
+
+      ulPageNums.id = 'idNum';
+      ulPageNums.className = 'num';
+
+      this._container.appendChild(ulPageNums);
+
+      function addNum(i) {
+        var num = doc.getElementById('idNum').appendChild(doc.createElement('li'));
+        num.innerHTML = i + 1;
+        num.onmouseover = function () {
+          timer = setTimeout(function () {
+            num.className = 'active';
+            that.Auto = false;
+            that.run(i);
+          }, 200);
+        };
+        num.onmouseout = function () {
+          clearTimeout(timer);
+          num.className = '';
+          that.Auto = true;
+          that.run();
+        };
+        pageNums[i] = num;
+      }
+
+      for (var i = 0; i < this._count; i++) {
+        addNum(i);
+      }
+    }
+  };
+
+  return {
+    init: function (container, options) {
+      var carouselInstance = new CarouselClass(container, options);
+      carouselInstance.addPageNumbers();
+      carouselInstance.run();
+    }
+  };
+
+})(window, document);
